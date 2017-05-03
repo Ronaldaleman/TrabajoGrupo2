25683ce26285991a42fd925d148f9e5716a05b25
Ronald <raleman@uca.edu.ni>
2017-05-03 10:14:50 -0700

    Se crea las clases insertarUsuario y la clase del servicio
    Se crearon las clases para insertar usuario y crear el servicio
    para validar la cedula

---
+++ Datos/usuarioDatos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class usuarioDatos
+    {
+        public void Insertar(Entidad.Usuarios a)
+        {
+            Entidad.BD_EvaluacionEntities dc = null;
+            try
+            {
+                dc = new Entidad.BD_EvaluacionEntities();
+                dc.Usuarios.Add(a);
+                dc.SaveChanges();
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+
+        public void UpdateUsuario(Entidad.Usuarios a)
+        {
+            Entidad.Usuarios usr = null;
+            Entidad.BD_EvaluacionEntities dc = null;
+            try
+            {
+                dc = new Entidad.BD_EvaluacionEntities();
+                usr = dc.Usuarios.Where(c => c.Usuarios1 == a.Usuarios1).FirstOrDefault();
+                if (usr != null)
+                {
+                    usr.Login = a.Login;
+                    usr.Nombre = a.Nombre;
+                    usr.Clave = a.Clave;
+                    usr.FechaProceso = a.FechaProceso;
+                    usr.Cedula = a.Cedula;
+                    dc.SaveChanges();
+                }
+            }
+            catch (Exception err)
+            {
+
+                throw new Exception("Error al ejecutar update en Usuarios////Detalle: " + err.Message);
+            }
+            finally
+            {
+                if (dc != null)
+                    dc.Dispose();
+            }
+        }
+
+        public void BajaUsuario(Entidad.Usuarios a)
+        {
+            Entidad.Usuarios usr = null;
+            Entidad.BD_EvaluacionEntities dc = null;
+            try
+            {
+                dc = new Entidad.BD_EvaluacionEntities();
+                usr = dc.Usuarios.Where(c => c.Usuarios1 == a.Usuarios1).FirstOrDefault();
+                if (usr != null)
+                {
+                    usr.Estado = 2;
+                    usr.FechaProceso = a.FechaProceso;
+                    dc.SaveChanges();
+                }
+            }
+            catch (Exception err)
+            {
+
+                throw new Exception("Error al ejecutar update en Usuarios////Detalle: " + err.Message);
+            }
+            finally
+            {
+                if (dc != null)
+                    dc.Dispose();
+            }
+        }
+    }
+}
---
+++ Negocio/webServicioNegocio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class webServicioNegocio
+    {
+        public string ValidaCedula(string numeroCedula)
+        {
+            try
+            {
+                ServiciosUca.ServiciosUcaClient ws = new ServiciosUca.ServiciosUcaClient();
+                string resp = (ws.ValidarCedula(numeroCedula));
+                return resp;
+            }
+            catch (Exception err)
+            {
+
+                throw (err);
+            }
+
+        }
+    }
+}
