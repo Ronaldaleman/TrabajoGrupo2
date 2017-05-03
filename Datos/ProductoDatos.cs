3db5f3cea6e0ac30ba26581dadd8231033a6e5c3
Sami Khaled Al Masagedi Castro <Sami Khaled Al Masagedi Castro>
2017-05-03 14:12:55 -0600

    Se crea clase ProductoDatos
    Se creo la clase ProductoDatos y dentro de esta tres metodos,
    InsertaProductoDatos, EditaProductoDatos, RetornaListaProductosDatos
    los cuales insertan, editan y retorna una lista de productos
    respectivamente.

---
+++ Datos/ProductoDatos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ProductoDatos
+    {
+        Entidad.BD_EvaluacionEntities dc = null;
+
+        public int InsertaProdructoDatos(Entidad.Productos producto)
+        {
+            try
+            {
+                dc = new Entidad.BD_EvaluacionEntities();
+                dc.Productos.Add(producto);
+                return dc.SaveChanges();
+            }
+            catch (Exception excepcion)
+            {
+                throw excepcion;
+            }
+        }
+
+        public int EditaProductoDatos(Entidad.Productos producto)
+        {
+            try
+            {
+                dc = new Entidad.BD_EvaluacionEntities();
+                dc.Productos.Attach(producto);
+                return dc.SaveChanges();
+            }
+            catch (Exception excepcion)
+            {
+                throw excepcion;
+            }
+        }
+
+        public List<Entidad.Productos> RetornaListaProductosDatos()
+        {
+            dc = new Entidad.BD_EvaluacionEntities();
+            return dc.Productos.ToList();
+            throw new Exception();
+        }    
+    }
+}
